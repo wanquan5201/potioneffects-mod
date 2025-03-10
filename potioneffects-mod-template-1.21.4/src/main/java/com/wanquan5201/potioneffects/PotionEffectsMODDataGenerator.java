bb3d951f18f65e1393e2668d185049ebafe1e177
wanquan5201 <118060316+wanquan5201@users.noreply.github.com>
2025-03-10 11:19:36 +0800

    Add files via upload

---
+++ potioneffects-mod-template-1.21.4/src/main/java/com/wanquan5201/potioneffects/PotionEffectsMODDataGenerator.java
@@ -0,0 +1,11 @@
+package com.wanquan5201.potioneffects;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PotionEffectsMODDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
